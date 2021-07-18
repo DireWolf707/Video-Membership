2cb907923e5366a56c43eab6d3ddf6e9fc01d255
DireWolf707 <rahulsood707@gmail.com>
2021-07-18 14:12:55 +0530

    added membership app

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,8 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# API testing keys Not live keys
+STRIPE_PUBLISHABLE_KEY = 'pk_test_51JETadSBRHaXAPZ2zCYEeMpMMpWOLlBXtSo5NhyhDQaZ4l4TO4tnkGkRXzLqmvufjxqwqG5v3RSeFlQtazyoIquW00Y1vlFXf4'
+STRIPE_SECRET_KEY = 'sk_test_51JETadSBRHaXAPZ2fM1N1Fv4SHclqf0piZJd7H9dacArVViTvBFRmBTzF0FIFXFndkwlbIO51TUr5oKHG0LWmfkG00VgirIR3R'
--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
-# Create your models here.
+
+class MembershipChoices(models.TextChoices):
+    Enterprise = 'ent'
+    Professional = 'pro'
+    Free = 'free'
+
+
+class Membership(models.Model):
+    title = models.CharField(max_length=25)
+    type = models.CharField(
+        max_length=4,
+        choices=MembershipChoices.choices,
+        default=MembershipChoices.Free,
+    )
+    price = models.IntegerField()
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return self.type
+
+
+class UserMembership(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='membership'
+    )
+    stripe_customer_id = models.CharField(max_length=40)
+    stripe_Subscription_id = models.CharField(max_length=40, blank=True)
+    active = models.BooleanField(default=True)
+    membership = models.ForeignKey(
+        Membership, related_name='members', on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self) -> str:
+        return str(self.user)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def post_save_usermembership(sender, instance, created, *args, **kwargs):
+    if created:
+        customer = stripe.Customer.create(
+            email=instance.email
+        )
+        free_membership = Membership.objects.get(type=MembershipChoices.Free)
+        UserMembership.objects.create(
+            user=instance,
+            stripe_customer_id=customer['id'],
+            membership=free_membership
+        )
